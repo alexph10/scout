4ce4b9da3504f27a4385b61d655dce3e31db29a0
alexph10 <pnhcan1@gmail.com>
2026-06-04 09:34:11 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,5 +5,4 @@ from .config import load_dotenv
 __version__ = "0.2.0"
 
 # Pick up GITHUB_TOKEN from ~/.scout/.env without requiring shell setup.
-# Real environment variables always win.
 load_dotenv()
